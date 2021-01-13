18be9c60ec00f9e6f5b3b089117de30630cf0817
ponlawat-w <ponlawat_w@cmu.ac.th>
2021-01-13 16:56:10 +0100

    Fix S2 Query

--- contracts/S2Regions.sol
+++ contracts/S2Regions.sol
@@ -5,4 +5,20 @@ import './Regions.sol';
 
 contract S2Regions is Regions {
   constructor() Regions(0x40, 0x80) {}
+
+  function query(uint64 cellID) public override view returns (RegionMetadata memory region) {
+    uint64 markBits =   0xfffffffffffffff8; // 1111 1111 ... 1111 1000
+    uint64 finalMark =  0x0000000000000004; // 0000 0000 ... 0000 0100
+    while (markBits > 0) {
+      uint8 regionId = getRegionIdFromExactCellID(cellID);
+      if (regionId > 0) {
+        return getRegionFromId(regionId).metadata;
+      }
+
+      cellID = (cellID & markBits) | finalMark;
+      markBits = markBits << 2;
+      finalMark = finalMark << 2;
+    }
+    return RegionMetadata({id: 0, registrar: address(0), name: ""});
+  }
 }
