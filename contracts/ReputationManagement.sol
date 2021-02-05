c17f721e0eb87942e46926072dd7b50d3f37bdab
ponlawat-w <ponlawat_w@cmu.ac.th>
2021-02-05 12:23:29 +0100

    Update Reputation to Service Context

--- contracts/ReputationManagement.sol
+++ contracts/ReputationManagement.sol
@@ -9,40 +9,62 @@ contract ReputationManagement {
   Regions private regionsContract;
 
   struct Reputation {
-    uint16 value; // 0 to 65535
-    int length;   // number of feedbacks
+    uint64 value; // 0x0 to 0xfffffffffffffffff
+    uint timestamp;
+    uint64[] records;
+    uint[] timestamps;
   }
 
-  // DeviceAddr => RegionID => Reputation
-  mapping(address => mapping(uint8 => Reputation)) private reputations;
+  // RegionID => DeviceAddr => ServiceName => Reputation
+  mapping(uint8 => mapping(address => mapping(uint32 => Reputation))) reputations;
 
   constructor(address devicesContractAddress) {
     devicesContract = Devices(devicesContractAddress);
     regionsContract = Regions(devicesContract.getRegionsContractAddress());
   }
 
-  function getReputation(address deviceAddr, uint8 regionID) public view returns (uint16 reputationValue) {
-    return reputations[deviceAddr][regionID].value;
+  function getReputationData(uint8 regionID, address deviceAddr, uint32 serviceName) public view returns (Reputation memory reputation) {
+    return reputations[regionID][deviceAddr][serviceName];
   }
 
-  function getReputation(address deviceAddr) public view returns (uint16 reptuationValue) {
+  function getReputationData(address deviceAddr, uint32 serviceName) public view returns (Reputation memory reputation) {
     uint8 regionID = regionsContract.query(devicesContract.getDeviceFromAddress(deviceAddr).location).id;
-    return getReputation(deviceAddr, regionID);
+    return getReputationData(regionID, deviceAddr, serviceName);
   }
 
-  function addFeedback(address deviceAddr, uint16 feedback) public {
+  function getReputationValue(uint8 regionID, address deviceAddr, uint32 serviceName) public view returns (uint64 reputationValue) {
+    return getReputationData(regionID, deviceAddr, serviceName).value;
+  }
+
+  function getReputationValue(address deviceAddr, uint32 serviceName) public view returns (uint64 reputationValue) {
+    return getReputationData(deviceAddr, serviceName).value;
+  }
+
+  function updateReputation(address deviceAddr, uint32 serviceName, uint64 reputationValue) public {
     uint8 regionID = regionsContract.query(devicesContract.getDeviceFromAddress(deviceAddr).location).id;
     require(regionID > 0);
     Regions.RegionMetadata memory regionMetadata = regionsContract.getRegionData(regionID).metadata;
     require(regionMetadata.registrar == msg.sender);
-    Reputation memory reputation = reputations[deviceAddr][regionID];
-    reputation.value = uint16(
-      (
-        (uint256(reputation.value) * uint256(reputation.length))
-        + feedback
-      ) / uint256(++reputation.length)
-    ); // ((rep * n) + f) / (n + 1) => instantly get new average value
 
-    reputations[deviceAddr][regionID] = reputation;
+    Reputation memory reputation = getReputationData(regionID, deviceAddr, serviceName);
+    reputation = addRecord(reputation, reputationValue, block.timestamp);
+    reputation.value = reputationValue;
+    reputation.timestamp = block.timestamp;
+    reputations[regionID][deviceAddr][serviceName] = reputation;
+  }
+
+  function addRecord(Reputation memory reputation, uint64 data, uint timestamp) private pure returns (Reputation memory) {
+    uint newLength = reputation.records.length + 1;
+    uint64[] memory newRecords = new uint64[](newLength);
+    uint[] memory newTimestamps = new uint[](newLength);
+    for (uint i = 0; i < reputation.records.length; i++) {
+      newRecords[i] = reputation.records[i];
+      newTimestamps[i] = reputation.timestamps[i];
+    }
+    newRecords[newLength - 1] = data;
+    newTimestamps[newLength - 1] = timestamp;
+    reputation.records = newRecords;
+    reputation.timestamps = newTimestamps;
+    return reputation;
   }
 }
